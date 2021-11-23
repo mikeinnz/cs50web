f3c81ec9d71b3db5e6386b856387edac0f9b8d38
Thac Ngo <nhthac@gmail.com>
2021-11-23 22:35:11 +1300

    add layout and login

--- capstone/capstone/settings.py
+++ capstone/capstone/settings.py
@@ -124,3 +124,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirect users to homepage after logging in or logging out
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'index'
--- capstone/inventory/views.py
+++ capstone/inventory/views.py
@@ -1,14 +1,21 @@
-from django.http.response import HttpResponse
+from django.contrib.auth import logout
+from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 
 def index(request):
     return HttpResponse("hello, world!")
 
 
-def login(request):
+def login_view(request):
 
     if request.method == "POST":
         return HttpResponse("POST")
     else:
         return render(request, "inventory/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
