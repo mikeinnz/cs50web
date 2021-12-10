6d25577b16607f6fffea65a18ba5fcd9c22a942f
Thac Ngo <nhthac@gmail.com>
2021-12-10 19:36:35 +1300

    settings for heroku

--- capstone/capstone/settings.py
+++ capstone/capstone/settings.py
@@ -27,7 +27,7 @@ env = environ.Env()
 environ.Env.read_env()
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
